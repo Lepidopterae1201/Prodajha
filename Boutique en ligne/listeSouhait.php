38d9d966cc33c5b442f73fdbd0106e6e16322d3d
Azdarhanos <tom.woodall99@gmail.com>
2021-03-16 12:55:23 +0100

    Page ListeSouhait
    Création de la page

--- Boutique en ligne/listeSouhait.php
+++ Boutique en ligne/listeSouhait.php
@@ -1,48 +1,87 @@
 <?php
 
 session_start();
-if($_SESSION == False) {
+if ($_SESSION == False) {
 	header("location:index.php");
-}else{
-require_once('modeles/ListeSouhait.php');
-$Resultat = new ListeSouhait();
-$resultat = $Resultat->afficherListeSouhait($_SESSION['idClient']);
-}
-
-include('header.html');
-$afficher_categories = false;
-require_once("modeles/Article.php");
-
-$Article = new Article()
+} else {
+	require_once('modeles/ListeSouhait.php');
+	$Resultat = new ListeSouhait();
+	$resultat = $Resultat->afficherListeSouhait($_SESSION['idClient']);
+	$afficher_categories = false;
 ?>
+	<script>
+		let disabled_map = new Map;
+	</script>
+	<?php include('header.html'); ?>
+	<html>
 
-<body>
-	<header>
-		<?php include('navbarHaut.php') ?>
-	</header>
-	<div class="container">
-		<div class="row">
-			<?php foreach ($Article as $article) { ?>
-			<div class="coll-md-6" style="max-width: 540px;">
-				<div class="row no-gitters">
-					<div class="col-4">
-						<a href="article.php?idart=<?php echo($article['idArticle']);?>">
-						<img class="card-img" src="<?php echo($article['image']) ?>">
-						</a>
-					</div>
-					<div class="col-8">
-						<div class="card-body">
-							<a href="article.php?idart=<?php echo($article['idArticle']);?>">
-								<h5 class="card-title"><?php echo($article['nom']) ?></h5>
-                      			<p class="card-text"><?php echo($article['prix']) ?> €</p>
-							</a>
-						</div>
-					</div>				
-				</div>
-			</div>
+	<body>
+		<header>
 			<?php
-			}
+			include('navbarHaut.php');
+			?>
+		</header>
+		<div class="col-md-8">
+			<?php if (empty($resultat)) { ?>
+				<ul class="list-group list group-flush">
+					<li class="list-group-item">
+						<div class="row">
+							<div class="col-sm-9 col-xs-12">
+								<h5 class="mr-auto">Votre liste de souhait est vide</h5>
+							</div>
+							<div class="col-sm-3 col-xs-12">
+								<a class="btn btn-warning ml-auto" href="index.php">Retour à l'accueil</a>
+							</div>
+						</div>
+					</li>
+				</ul>
+				<script>
+					disabled_map.set("nothing", true);
+				</script>
+				<?php
+			} else {
+				foreach ($resultat as $article) { ?>
+					<div id="article<?php echo ($article['idArticle']) ?>" class="card" style="margin-bottom: 5px;">
+						<div class="card-header">
+							<h5 class="card-title"><?php echo ($article['nom']) ?></h5>
+						</div>
+						<div class="card-body">
+							<div class="container-fluid">
+								<div class="row">
+									<div class="col-5 col-md-3">
+										<img src="<?php echo ($article['image']) ?>" class="img-fluid">
+									</div>
+									<div class="col-7 col-md-6">
+										<div class="row">
+											<div class="col-12 col-ld-4">
+												<form id="idA_delete<?php echo ($article['idArticle']) ?>" class="deleteForm" method="POST" action="#">
+													<button class="btn btn-outline-secondary btn-sm" type="submit">supprimer</button>
+												</form>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				<?php } ?>
+			<?php }
 			?>
 		</div>
-	</div>
-</body>
+	</body>
+
+	</html>
+
+	<style type="text/css">
+		img {
+			max-height: 150px;
+		}
+
+		.content {
+			margin: 10px;
+		}
+	</style>
+
+	<script src="static/jquery-3.5.1.min.js"></script>
+
+<?php } ?>
