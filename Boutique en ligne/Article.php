b66db80d6b7f0de1ba66eb96cf355c6778583bb1
Azdarhanos <tom.woodall99@gmail.com>
2021-03-16 09:54:43 +0100

    Ajax Liste de Souhait
    Finition de l'ajax pour ajouter un produit à la liste de souhait

--- Boutique en ligne/Article.php
+++ Boutique en ligne/Article.php
@@ -75,18 +75,16 @@ include('header.html')
                   <div id="articleSouhait">
                     <div class="col-auto">
                       <input id="idArt" type="text" hidden="True" name="idart" value=<?php echo ($art['idArticle']); ?>>
-                      <?php if ($souhait == True) { ?>
-                        <button id="Souhait" class="btn btn-warning" type="submit">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                          </svg>
-                        </button> <?php
-                                                                                                                              } else { ?>
-                        <button id="Souhait" class="btn btn-warning" type="submit">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
-                            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
-                          </svg>
-                        </button> <?php } ?>
+                      <button id="souhait_fill" class="btn btn-warning Souhait d-none" type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                          <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                        </svg>
+                      </button>
+                      <button id="souhait_empty" class="btn btn-warning Souhait d-none" type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                          <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                        </svg>
+                      </button>
                     </div>
                   </div>
                 </div>
@@ -251,7 +249,7 @@ include('header.html')
   });
 
   $(document).ready(function() {
-    $("#Souhait").on('click', function() {
+    $(".Souhait").on('click', function() {
       var idart = $('#idArt').val();
       var idclient = <?php echo $idClient ?>;
       $.ajax({
@@ -263,11 +261,12 @@ include('header.html')
         },
         dataType: "json",
         success: function(data) {
-          console.log(data.action);
-          if (action = 'del') {
-            document.getElementById("articleSouhait").children = '<svg xmlns = "http://www.w3.org/2000/svg" width = "16" height = "16" fill = "currentColor" class = "bi bi-star" viewBox = "0 0 16 16"> <path d = "M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" / > </svg>'
-          }else if(action = 'add'){
-            document.getElementById("articleSouhait").children = '<svg xmlns="http: //www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16"> <path d = "M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" / > </svg>';
+          document.querySelector("#souhait_fill").classList.add("d-none");
+          document.querySelector("#souhait_empty").classList.add("d-none");
+          if (data.action == 'del') {
+            document.querySelector("#souhait_empty.d-none").classList.remove("d-none");
+          } else if (data.action == 'add') {
+            document.querySelector("#souhait_fill.d-none").classList.remove("d-none");
           }
         },
         error: function() {
@@ -277,4 +276,10 @@ include('header.html')
     })
   })
 
+  <?php if ($souhait == TRUE) { ?>
+    document.getElementById("souhait_fill").classList.remove("d-none");
+  <?php } else { ?>
+    document.getElementById("souhait_empty").classList.remove("d-none");
+  <?php }
+  ?>
 </script>
